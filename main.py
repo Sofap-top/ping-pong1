225b29f7f9ced69ee47cef7a0ecb6ca41de2f5be
Sofap-top <sofap1097@gmail.com>
2025-04-11 11:31:54 +0300

    Final version 'Ping-pong'

--- main.py
+++ main.py
@@ -41,6 +41,8 @@ racketka2 = Player('r1-Photoroom.png',645,410,5,60,150)
 mach = Spr('tennis.png',350,250,7,65,65)
 f = False
 b = True
+speed_x = 3
+speed_y = 3
 while b:
     if f != True:
         w.blit(a,(0,0))
@@ -48,7 +50,21 @@ while b:
         racketka1.ret()
         racketka2.update_r() 
         racketka2.ret()
-        mach.ret()       
+        mach.ret()
+        mach.rect.x += speed_x
+        mach.rect.y += speed_y
+        if sprite.collide_rect(racketka1,mach) or sprite.collide_rect(racketka2,mach):
+            speed_x *= -1
+        if mach.rect.y < 0 or mach.rect.y > 435:
+            speed_y *= -1
+        if mach.rect.x < 0:
+            o =f1.render('Left player lose!',True,(230, 9, 196))
+            w.blit(o,(350,250))
+            f = True
+        if mach.rect.x > 635:
+            o1 =f1.render('Right player lose!',True,(230, 9, 196))
+            w.blit(o1,(350,250))
+            f = True
     for i in event.get():
         if i.type == QUIT:
             b = False
